07542b17fa5eab4853b6ecf2b812b8575680b261
unknown <anisspeed21@gmail.com>
2022-03-18 19:47:54 +0100

    removed scroll button

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <ScrollToTop />
         <Header />
         <Layout /> 
-        <ScrollButton />
+        {/* <ScrollButton /> */}
       </HandleData>
     </ThemeProvider>
     </>
